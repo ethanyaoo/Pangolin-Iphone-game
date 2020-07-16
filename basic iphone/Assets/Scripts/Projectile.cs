7a125880e476885930bd56baf96cf7257777e4f2
ethan <ey709@nyu.edu>
2020-07-16 10:48:23 -0400

    PG-2-1
    Commit errors

--- basic iphone/Assets/Scripts/Projectile.cs
+++ basic iphone/Assets/Scripts/Projectile.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class Projectile : MonoBehaviour
 {
     public Transform firingTarget;
-<<<<<<< HEAD
 
     public ParticleSystem breakEffect;
-=======
->>>>>>> 65ab40b6c91b266d820b05d247595b7782c49ebc
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider collider)
     {
         collider.gameObject.SetActive(false);
         gameObject.SetActive(false);
-<<<<<<< HEAD
 
         Instantiate(breakEffect, collider.transform.position, Quaternion.identity);
-=======
->>>>>>> 65ab40b6c91b266d820b05d247595b7782c49ebc
     }
 
     // Update is called once per frame
