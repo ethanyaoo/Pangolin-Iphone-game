3551c1c74eeee05a4af985909489578cc8884232
ethan <ey709@nyu.edu>
2020-07-16 11:03:35 -0400

    PG-2-2
    Fixed Commit errors

--- basic iphone/Assets/Scripts/Projectile.cs
+++ basic iphone/Assets/Scripts/Projectile.cs
@@ -7,12 +7,24 @@ public class Projectile : MonoBehaviour
     public Transform firingTarget;
 
     public ParticleSystem breakEffect;
+    ParticleSystem.MainModule breakMain;
+
+    private void Start() 
+    {
+        ParticleSystem.MainModule breakMain = breakEffect.main;
+    }
 
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider collider)
     {
         collider.gameObject.SetActive(false);
         gameObject.SetActive(false);
+
+        breakMain.startColor = new Color(
+                                            (float)Random.Range(0, 255),
+                                            (float)Random.Range(0, 255),
+                                            (float)Random.Range(0, 255)
+        );
 
         Instantiate(breakEffect, collider.transform.position, Quaternion.identity);
     }
